a84889ec2543389e31127ed0c6169e3860447e4c
Kauê M Sumariva <144291835+KMZ7878@users.noreply.github.com>
2023-10-22 18:58:03 -0300

    Opção de sair do sistema adicionado

--- main.c
+++ main.c
@@ -125,6 +125,7 @@ int main()
         printf("\t1 - Registrar nomes\n");
         printf("\t2 - Consultar nomes\n");
         printf("\t3 - Deletar nomes\n");
+        printf("\t4 - Sair do sistema\n\n");
         printf("opcao: "); //fim do menu
 
         scanf("%d", &opcao); //armazenando a escolha do usuario
@@ -142,6 +143,11 @@ int main()
         case 3:
             deletar_nomes();
             break;
+        case 4: 
+        printf("Obrigado por utilizar o sistema!\n");
+        return 0;
+        break;
+
         default:
             printf("Essa opção existe.\n");
             system("pause");
